aa42d918b2ba719085d1f75d0d08ece18a77ce25
Bao Bui <bvqbao@gmail.com>
2022-10-27 20:59:05 +0700

    Create a vue 3 project template

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+	<main
+		class="flex flex-col items-center w-full h-full min-h-screen p-12 bg-gray-100"
+	>
+		<h1>This is the home page</h1>
+	</main>
+</template>
+
+<style></style>
